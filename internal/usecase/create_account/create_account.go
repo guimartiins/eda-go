22c77bb31e789c437d960678a7ba7069dbb06c2c
Guilherme Martins <academicgmag@gmaiil.com>
2025-04-28 19:13:45 -0300

    add create account usecase test

---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,42 @@
+package createaccount
+
+import (
+	"github.com/guimartiins/fcutils/internal/entity"
+	"github.com/guimartiins/fcutils/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (u *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := u.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+
+	account := entity.NewAccount(client)
+
+	err = u.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{ID: account.ID}, nil
+}
